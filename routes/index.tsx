8fe313bedfcf4a33e548c07c45486d02698e5e6f
will-lol <will.bradshaw50@gmail.com>
2022-12-28 12:47:02 +1100

    add fresh badge

--- routes/index.tsx
+++ routes/index.tsx
@@ -10,7 +10,8 @@ export default function Home() {
       </Head>
       <body class="flex items-center justify-center h-screen flex-col p-4">
         <h1 class="text-6xl mb-4 text-center">Welcome to exhibition.rocks</h1>
-        <p class="text-center">This website is designed to be launched from a QR code at our exhibition sites.</p>
+        <p class="text-center pb-4">This website is designed to be launched from a QR code at our exhibition sites.</p>
+        <a href="https://fresh.deno.dev/"><img src="https://fresh.deno.dev/fresh-badge.svg" alt="Fresh badge" /></a>
       </body>
     </>
   );
