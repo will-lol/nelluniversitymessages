96ee2e01d6eee0252acc278d3785b0be6f85469d
will-lol <will.bradshaw50@gmail.com>
2022-12-29 20:02:03 +1100

    fix word wrap

--- components/ExhibitGrid/Exhibit.tsx
+++ components/ExhibitGrid/Exhibit.tsx
@@ -13,10 +13,10 @@ interface Props {
 
 export default function Exhibit(props: Props) {
     return (
-        <div id={props.id} class="w-full min-h-[calc(100svh-2rem)] bg-white shadow-exhibit border border-black border-opacity-30 p-6 m-4">
+        <div id={props.id} class="whitespace-pre-wrap w-full min-h-[calc(100svh-2rem)] bg-white shadow-exhibit border border-black border-opacity-30 p-6 m-4">
             <h1 class="text-4xl break-all mb-4 font-medium">{props.uuid}</h1>
             <h2 class="text-xl font-medium mb-4"><em>{props.title}</em>, <span class="font-normal">{timeElapsedString(props.date)} at {props.from.name}</span></h2>
-            <pre class="text-base font-sans">{props.children}</pre>
+            <pre class="whitespace-pre-wrap text-base font-sans">{props.children}</pre>
         </div>
     );
 }
